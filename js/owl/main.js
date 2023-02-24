4ccbb0589d1b09186f11bf900b4065fce5f2447f
Perkles <josuesousa1020@gmail.com>
2023-02-23 21:39:43 -0300

    sea commir

---
+++ js/owl/main.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+// const modal = document.querySelector('.modal-container')
+
+// function openModal(){
+//     modal.classList.add('active')
+
+// }
+// function closeModal(){
+//     modal.classList.remove('active')
+// }
