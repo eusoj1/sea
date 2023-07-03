3926bbb397ae15832818d8d573956229085bb41e
Perkles <josuesousa1020@gmail.com>
2023-07-03 07:20:17 -0300

    atualizacao-julho

---
+++ java.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.logo-2',{
+    rotate: { x: 0, y: 80, z: 0},
+    duration: 3000
+});
+sr.reveal('.cont-left', { duration: 3000});
+sr.reveal('.cont-right', { duration: 3000});
+sr.reveal('.tittle-cont-5', { duration: 3000});
+sr.reveal('.videos', { duration: 3000});
+sr.reveal('.tittle-cont-10', { duration: 3000});
+sr.reveal('.videos2', { duration: 1000});
+sr.reveal('.SECTION-7', { duration: 1000});
+sr.reveal('.cont50', { duration: 1000});
+sr.reveal('.cont-9', { duration: 1000});
+
+
+// .tittle-cont-5, .videos,
+//  .tittle-cont-10, .videos2, .SECTION-7, .cont50, .cont-9 {
